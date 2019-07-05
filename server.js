48d23e8bcff908b85bc43dea86fad0de1d588467
Arnab <arnab.dey@mail.polimi.it>
2019-07-05 11:17:48 +0200

    addedfinal

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser'); 4
+
+const bodyParser = require('body-parser');
 const db = require('./config/db');
 const port = process.env.PORT || 3000
 
